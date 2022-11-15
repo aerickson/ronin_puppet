2bd4331544de2a89eb21511314f61c03ca4a59f0
Andrew Erickson <aerickson@mozilla.com>
2022-11-14 16:10:03 -0800

    sync with non-testing branch

--- modules/linux_packages/files/py3915_install.sh
+++ modules/linux_packages/files/py3915_install.sh
@@ -5,7 +5,9 @@ set -e
 
 # TODO: check that this is run as root
 
-apt remove libpython3.9-minimal libpython3.9-stdlib -y
+# remove previous versions of these as they mess with installation
+# TODO: check exit code? 100 is ok - means not present...
+apt remove libpython3.9-minimal libpython3.9-stdlib -y || true
 
 # order matters
 #   created with https://github.com/starkandwayne/install-debs-in-order
@@ -20,7 +22,12 @@ dpkg -i libpython3.9-dev_3.9.15-1+bionic1_amd64.deb
 dpkg -i python3.9-venv_3.9.15-1+bionic1_amd64.deb
 dpkg -i python3.9-dev_3.9.15-1+bionic1_amd64.deb
 
-# changing /usr/bin/python3 makes `apt update` unhappy, fix.
+# changing /usr/bin/python3 makes various things unhappy due to dynamic lib naming
+# - `apt update`
+# - gnome-terminal (used to launch worker-runner)
+#
+# takeaways: we shouldn't change the python version (/usr/bin/python3 from 3.6 to 3.9)
+#   the system uses in the future.
 #
 # Traceback (most recent call last):
 #   File "/usr/lib/cnf-update-db", line 8, in <module>
@@ -30,7 +37,11 @@ dpkg -i python3.9-dev_3.9.15-1+bionic1_amd64.deb
 # ModuleNotFoundError: No module named 'apt_pkg'
 #
 cd /usr/lib/python3/dist-packages
-cp apt_pkg.cpython-36m-x86_64-linux-gnu.so apt_pkg.so
+ln -s apt_pkg.cpython-36m-x86_64-linux-gnu.so apt_pkg.so
+cd -
+
+cd /usr/lib/python3/dist-packages/gi
+ln -s _gi.cpython-36m-x86_64-linux-gnu.so _gi.so
 cd -
 
 apt install --fix-broken -y
