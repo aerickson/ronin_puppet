0489bf99d2fcbcae948bab59e554eb0c90e8741e
Andrew Erickson <aerickson@mozilla.com>
2023-02-07 16:02:37 -0800

    reformat ntp class, add 2204 block with fail()

--- modules/roles_profiles/manifests/profiles/ntp.pp
+++ modules/roles_profiles/manifests/profiles/ntp.pp
@@ -3,36 +3,51 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class roles_profiles::profiles::ntp {
+  case $facts['os']['name'] {
+    'Darwin': {
+      class { 'macos_ntp':
+        enabled    => true,
+        ntp_server => lookup('ntp_server'),
+      }
+    }
+    'Ubuntu': {
+      case $facts['os']['release']['full'] {
+        '18.04': {
+          $ntp_server = lookup('ntp_server', String)
+          class { 'ntp':
+            servers => [$ntp_server],
+          }
+        }
+        '22.04': {
+          # not working
+          # $ntp_server = lookup('ntp_server', String)
+          # class { 'ntp':
+          #     servers => [$ntp_server]
+          # }
 
-    case $::operatingsystem {
-        'Darwin': {
-            class { 'macos_ntp':
-                enabled    => true,
-                ntp_server => lookup('ntp_server'),
-            }
-        }
-        'Ubuntu': {
-            $ntp_server = lookup('ntp_server', String)
-            class { 'ntp':
-                servers => [$ntp_server]
-            }
-        }
-        'Windows': {
-        # https://bugzilla.mozilla.org/show_bug.cgi?id=1510754
-        # For windowstime resoucre timezone and server needs to be set in the same class
-        # Resource from ncorrare-windowstime
-            if $facts['custom_win_location'] == 'datacenter' {
-                $ntpserver = lookup('windows.datacenter.ntp')
-            } else {
-                $ntpserver = lookup('windows.external.ntp')
-            }
-            class { 'windowstime':
-                servers  => { "${ntpserver}" => '0x08'},
-                timezone => 'Greenwich Standard Time',
-            }
+          fail("Not implemeted for 2204 ${facts['os']['release']['full']}")
         }
         default: {
-            fail("${::operatingsystem} not supported")
+          fail("Unrecognized Ubuntu version ${facts['os']['release']['full']}")
         }
+      }
     }
+    'Windows': {
+      # https://bugzilla.mozilla.org/show_bug.cgi?id=1510754
+      # For windowstime resoucre timezone and server needs to be set in the same class
+      # Resource from ncorrare-windowstime
+      if $facts['custom_win_location'] == 'datacenter' {
+        $ntpserver = lookup('windows.datacenter.ntp')
+      } else {
+        $ntpserver = lookup('windows.external.ntp')
+      }
+      class { 'windowstime':
+        servers  => { "${ntpserver}" => '0x08' },
+        timezone => 'Greenwich Standard Time',
+      }
+    }
+    default: {
+      fail("${facts['os']['name']} not supported")
+    }
+  }
 }
